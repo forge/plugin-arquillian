a4a19b4d95d02ccfe3fece8c719d928f9a82810a
Paul Bakker <paul.bakker.nl@gmail.com>
2011-06-28 21:33:47 +0200

    Existing Arquillian.xml is not overwritten any more.

--- src/main/java/org/jboss/seam/forge/arquillian/container/Jboss7Managed.java
+++ src/main/java/org/jboss/seam/forge/arquillian/container/Jboss7Managed.java
@@ -15,48 +15,90 @@ import org.jboss.shrinkwrap.descriptor.spi.Node;
 import javax.inject.Inject;
 import java.util.List;
 
-public class Jboss7Managed implements Container {
-    @Inject
-    ProfileBuilder builder;
+public class Jboss7Managed implements Container
+{
+   @Inject
+   ProfileBuilder builder;
 
-    @Inject
-    Project project;
+   @Inject
+   Project project;
 
-    @Inject
-    Shell shell;
+   @Inject
+   Shell shell;
 
-    @Override
-    public void installDependencies(String arquillianVersion) {
-        DependencyFacet dependencyFacet = project.getFacet(DependencyFacet.class);
+   @Override
+   public void installDependencies(String arquillianVersion)
+   {
+      Dependency dependency = createDependency();
 
-        DependencyBuilder dep1 = DependencyBuilder.create()
-                .setGroupId("org.jboss.as")
-                .setArtifactId("jboss-as-arquillian-container-managed")
-                .setScopeType(ScopeType.TEST);
+      builder.addProfile("arq-jbossas-7-managed", dependency);
 
-        List<Dependency> dependencies = dependencyFacet.resolveAvailableVersions(dep1);
+      String jbossHome = shell.promptCommon("What is your JBoss home?", PromptType.FILE_PATH);
 
-        if(dependencies.isEmpty()) {
-            throw new RuntimeException("Dependency " + dep1.toCoordinates() + " could not be resolved. Does your POM contain the correct repositories?");
-        }
-        Dependency dependency = shell.promptChoiceTyped("Which version of JBoss AS 7 do you want to use?", dependencies, dependencies.get(dependencies.size() - 1));
+      ResourceFacet resources = project.getFacet(ResourceFacet.class);
+      FileResource<?> resource = (FileResource<?>) resources.getResourceFolder().getChild("arquillian.xml");
+      if (resource.exists())
+      {
+         editExistingArquillianConfig(jbossHome, resource);
+      } else
+      {
+         createNewArquillianConfig(jbossHome, resource);
+      }
+   }
 
-        builder.addProfile("arq-jbossas-7-managed", dependency);
+   private Dependency createDependency()
+   {
+      DependencyFacet dependencyFacet = project.getFacet(DependencyFacet.class);
 
+      DependencyBuilder dep1 = DependencyBuilder.create()
+              .setGroupId("org.jboss.as")
+              .setArtifactId("jboss-as-arquillian-container-managed")
+              .setScopeType(ScopeType.TEST);
 
-        String jbossHome = shell.promptCommon("What is your JBoss home?", PromptType.FILE_PATH);
+      List<Dependency> dependencies = dependencyFacet.resolveAvailableVersions(dep1);
 
-        Node xml = XMLParser.parse("<arquillian xmlns=\"http://jboss.org/schema/arquillian\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-                "            xsi:schemaLocation=\"http://jboss.org/schema/arquillian http://jboss.org/schema/arquillian/arquillian_1_0.xsd\"></arquillian>");
+      if (dependencies.isEmpty())
+      {
+         throw new RuntimeException("Dependency " + dep1.toCoordinates() + " could not be resolved. Does your POM contain the correct repositories?");
+      }
 
-        Node container = xml.create("container").attribute("qualifier", "jboss").attribute("default", "true");
-        container.create("configuration").create("property").attribute("name", "jbossHome").text(jbossHome);
-        container.create("protocol").attribute("type", "jmx-as7").create("property").attribute("name", "executionType").text("REMOTE");
+      return shell.promptChoiceTyped("Which version of JBoss AS 7 do you want to use?", dependencies, dependencies.get(dependencies.size() - 1));
+   }
 
-        ResourceFacet resources = project.getFacet(ResourceFacet.class);
-        FileResource<?> resource = (FileResource<?>)resources.getResourceFolder().getChild("arquillian.xml");
-        resource.setContents(XMLParser.toXMLString(xml));
+   private void createNewArquillianConfig(String jbossHome, FileResource<?> resource)
+   {
+      Node xml = XMLParser.parse("<arquillian xmlns=\"http://jboss.org/schema/arquillian\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+              "            xsi:schemaLocation=\"http://jboss.org/schema/arquillian http://jboss.org/schema/arquillian/arquillian_1_0.xsd\"></arquillian>");
+      addJbossContainer(jbossHome, xml);
+      resource.setContents(XMLParser.toXMLString(xml));
+   }
 
+   private void editExistingArquillianConfig(String jbossHome, FileResource<?> resource)
+   {
+      boolean containerExists = false;
 
-    }
+      Node existing = XMLParser.parse(resource.getResourceInputStream());
+      for (Node node : existing.children())
+      {
+         if (node.name().equals("container") && node.attribute("qualifier").equals("jboss"))
+         {
+            containerExists = true;
+            shell.println("JBoss container exists in Arquillian.xml, no changes were made");
+            break;
+         }
+      }
+
+      if (!containerExists)
+      {
+         addJbossContainer(jbossHome, existing);
+         resource.setContents(XMLParser.toXMLString(existing));
+      }
+   }
+
+   private void addJbossContainer(String jbossHome, Node xml)
+   {
+      Node container = xml.create("container").attribute("qualifier", "jboss").attribute("default", "true");
+      container.create("configuration").create("property").attribute("name", "jbossHome").text(jbossHome);
+      container.create("protocol").attribute("type", "jmx-as7").create("property").attribute("name", "executionType").text("REMOTE");
+   }
 }
